9582b4b9f4eaf27f6f009f7c337198c1d141850d
unknown <brunolee04@gmail.com>
2022-11-18 18:14:42 -0300

    18/11

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,32 +3,67 @@ import './style.css';
 
 
 export function Card(){
+
+    const urlImgPrefix = 'https://codevalley.dev/games/guess-who/image/models';
+
+    const person = [1,random(0,2),random(1,10),random(0,8),random(1,3),0,0,random(0,4),random(0,5),random(1,4),random(0,2),random(0,3)];
+    //const person = ['sexo','masculino','preto','careca'];
+
+   
+
+    const earsImg     = <img src={`${urlImgPrefix}/1/${person[1]}/ears/1.fw.png`}/> ;
+    const eyesImg     = person[2]<1?'':<img src={`${urlImgPrefix}/1/${person[1]}/eyes/${person[2]}.fw.png`}/>;
+    const hairImg     = person[3]<1?'':<img src={`${urlImgPrefix}/1/${person[1]}/hair/${person[3]}.fw.png`}/>;
+    const nooseImg    = person[4]<1?'':<img src={`${urlImgPrefix}/1/${person[1]}/noose/${person[4]}.fw.png`}/>;
+    const beardImg    = person[7]<1?'':<img src={`${urlImgPrefix}/1/${person[1]}/beard/${person[7]}.fw.png`}/>;
+    const mustacheImg = person[8]<1?'':<img src={`${urlImgPrefix}/1/${person[1]}/mustache/${person[8]}.fw.png`}/>;
+    const eyebrowImg  = person[9]<1?'':<img src={`${urlImgPrefix}/1/${person[1]}/eyebrow/${person[9]}.fw.png`}/>;
+    const glassesImg  = person[10]<1?'':<img src={`${urlImgPrefix}/1/${person[1]}/glasses/${person[10]}.fw.png`}/>;
+    const hatImg      = person[11]<1?'':<img src={`${urlImgPrefix}/1/${person[1]}/hat/${person[11]}.fw.png`}/>;
+    
+    
+
+
     return (
             
                 <div className="card">
                     <div className='personImg'>
                         
                         <div className='ears'>
-                            <img src='https://dld.com.br/images/guess-who/models/orelha/1.fw.png'/>
+                            {earsImg}
                         </div>
                         <div className='head'>
-                            <img src='https://dld.com.br/images/guess-who/models/cabeca/1.fw.png'/>
-                        </div>
-                        <div className='noose'>
-                            <img src='https://dld.com.br/images/guess-who/models/nariz/1.fw.png'/>
+                            <img src={`${urlImgPrefix}/1/${person[1]}/head/1.fw.png`}/>
                         </div>
                         <div className='eyes'>
-                            <img src='https://dld.com.br/images/guess-who/models/olhos/1.fw.png'/>
+                            {eyesImg}
                         </div>
+                        <div className='noose'>
+                            {nooseImg}
+                        </div>
+                        
                         <div className='eyebrow'>
-                            <img src='https://dld.com.br/images/guess-who/models/sobrancelha/1.fw.png'/>
+                            {eyebrowImg}
                         </div>
                         <div className='beard'>
-                            <img src='https://dld.com.br/images/guess-who/models/barba/1.fw.png'/>
+                            {beardImg}
                         </div>
                         <div className='mouth'>
-                            <img src='https://dld.com.br/images/guess-who/models/boca/1.fw.png'/>
+                            <img src={`${urlImgPrefix}/1/${person[1]}/mouth/1.fw.png`}/>
                         </div>
+                        <div className='mustache'>
+                           {mustacheImg}
+                        </div>
+                        <div className='hair'>
+                           {hairImg}
+                        </div>
+                        <div className='glasses'>
+                           {glassesImg}
+                        </div>
+                        <div className='hat'>
+                           {hatImg}
+                        </div>
+                        
                         
                     </div>
                     <p className="card__name">Tubá</p>
@@ -37,4 +72,9 @@ export function Card(){
                 </div>
            
     );
+
+
+    function random(min,max){
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 }
